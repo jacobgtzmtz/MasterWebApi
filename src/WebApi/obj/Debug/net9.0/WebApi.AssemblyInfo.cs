40f8ca3f859c8e544f58f5749a4cff21ff346ebf
Jacob Gutierrez <jacobgtzmtz@gmail.com>
2026-01-30 17:45:54 -0600

    ensamblados

--- src/WebApi/obj/Debug/net9.0/WebApi.AssemblyInfo.cs
+++ src/WebApi/obj/Debug/net9.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb6b66284f1b50949e9d56500a4ed0c156e4c460")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
